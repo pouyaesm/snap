f57452a63927ab1aa4de65c370ab04538298b807
pouyaesm <pouya.esmailian@gmail.com>
2019-06-01 11:41:48 +0100

    variable name improved, comment corrected

--- snap-adv/r2v_word2vec.cpp
+++ snap-adv/r2v_word2vec.cpp
@@ -140,15 +140,15 @@ void TrainModel(TVVec<TInt, int64>& WalksVV, TIntIntH& RnmH, TIntIntH& RnmBackH,
 					// Additionally sample other "in" roles of the original output word (Word)
 					// For role count = 4, words with ids  3 x w + [1, 2, 3]
 			    // are "in" roles of base word w. For example, if 2in+ is close to 4in-,
-			    // 2in+ should be far from 2in- (other role of input word '2')
+			    // 2in+ should be far from 4in+ (other role of output word '4')
 					if(roleNegativeSampling && WordRole != 0 && WordRole != n){
-						int64 originalId = baseWord * roleCount + n;
-						if(!RnmH.IsKey(originalId)){
+						int64 otherRoleId = baseWord * roleCount + n;
+						if(!RnmH.IsKey(otherRoleId)){
 //							printf("Sample [%d]: other role %d not found for %d[%d]\n", n + 1,
 //							originalId,  CurrWordOriginal, CurrWord);
 							continue;
 						}
-						Target = RnmH.GetDat(originalId);
+						Target = RnmH.GetDat(otherRoleId);
 //						printf("%d (other roles), ", RnmBackH.GetDat(Target));
 						Label = 0;
 					}else{
